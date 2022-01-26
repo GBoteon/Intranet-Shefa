d82457816b40062b6fd2e7949660dafa91f4289b
Gustavo <boteongustavo@gmail.com>
2022-01-26 10:01:17 -0300

    Create repository

---
+++ Dev.Shefa/Dev.Shefa/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using Dev.Shefa.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Dev.Shefa.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Rh()
+        {
+            return View();
+        }
+        public IActionResult Controladoria()
+        {
+            return View();
+        }
+        public IActionResult Financeiro()
+        {
+            return View();
+        }
+        public IActionResult Loja()
+        {
+            return View();
+        }
+        public IActionResult Marketing()
+        {
+            return View();
+        }
+        public IActionResult TI()
+        {
+            return View();
+        }
+        public IActionResult ST()
+        {
+            return View();
+        }
+        public IActionResult QA()
+        {
+            return View();
+        }
+        public IActionResult AgendarSala()
+        {
+            return View();
+        }
+        public IActionResult AgendarEquipamento()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Dev.Shefa/Dev.Shefa/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Dev.Shefa.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Dev.Shefa.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+        }
+    }
+}
